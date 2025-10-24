598200363a03351b8be69e413bd4ab31f337d256
Ekaterina Grishina <grischina.kotya17@gmail.com>
2025-10-24 14:59:27 +0200

    Add routes and api requests

--- src/taskpane/api/routes.ts
+++ src/taskpane/api/routes.ts
@@ -3,3 +3,13 @@ export const PLUGIN_CONTRACT_RECOMMENDATION_GENERAL = "/v1/plugin/contract/recom
 export const PLUGIN_CONTRACT_RECOMMENDATION_CUSTOM = "/v1/plugin/contract/manual-recommendation";
 
 export const PLUGIN_AUTH_CHECK_ACCESS = "/v1/auth-b2b/check-access";
+export const PLUGIN_AUTH_CREATE_ACCESS = "/v1/auth-b2b/create";
+
+export const PLUGIN_AUTH_CLIENT_EXISTS = "/v1/client";
+
+export const PLUGIN_AUTH_OTP_SEND = "/v1/auth";
+export const PLUGIN_AUTH_OTP_VERIFY = "/v1/auth/verify";
+
+export const PLUGIN_AUTH_JWT_REFRESH = "/v1/auth/jwt/refresh";
+export const PLUGIN_AUTH_JWT_LOGOUT = "/v1/auth/jwt/logout";
+export const PLUGIN_AUTH_JWT_CLIENT_DATA = "/v1/auth/jwt/me";
